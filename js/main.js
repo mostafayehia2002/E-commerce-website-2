d3ae2fa05cab20c5ee99a684febe395bab3d1036
teefa <gad993813@gmail.com>
2023-02-13 22:37:45 +0200

    products

--- js/main.js
+++ js/main.js
@@ -22,3 +22,34 @@ closeMenu.onclick=()=>{
     menu.classList.toggle("translate");
 }
 
+//fetch oroduct from server
+let products=document.querySelector(".products");
+
+fetch("productData/product.json").then(
+    (request)=>{return request.json()}
+).then(
+    (data)=>{        
+        for(let i=0;i<4;i++){
+            let random=Math.floor(Math.random()*data.length);
+            products.innerHTML+=
+            `<div class="card">  
+            <img src="${data[random].product_img}" alt="" class="product-img"> 
+             <span class="product-brand">${data[random].product_brand}</span>
+             <h3 class="product-name">${data[random].product_name}</h3>
+             <div class="product-stars">
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+             </div>
+             <p class="product-price">${data[random].product_price}</p>
+             <div class="product-buy">
+               <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+             </div>
+           </div>`;
+           
+
+        }
+    }
+)
