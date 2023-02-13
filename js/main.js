7e31de3900d9d5c91ff6b67605161b0a8b157f50
teefa <gad993813@gmail.com>
2023-02-13 17:19:51 +0200

    2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+//navbar in large screen
 let list=document.querySelectorAll(".list li");
 let border=document.querySelectorAll(".list li span");
 list.forEach(function(e,i){  
@@ -10,3 +10,15 @@ list.forEach(function(e,i){
         border[i].classList.add("active");
     }
 })
+
+//navbar in mobile screen 
+let menu=document.querySelector(".navbar .list");
+let menuButton=document.querySelector(".mobile-list .menu");
+menuButton.onclick=()=>{
+    menu.classList.toggle("translate");
+}
+let closeMenu=document.getElementById("close");
+closeMenu.onclick=()=>{
+    menu.classList.toggle("translate");
+}
+
