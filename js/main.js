d9b357b2e3a3830a4d78597890be88d915a3f2d7
teefa <gad993813@gmail.com>
2023-02-15 23:28:46 +0200

    shop

--- js/main.js
+++ js/main.js
@@ -23,9 +23,9 @@ closeMenu.onclick=()=>{
 }
 
 //add year to footer 
-let date = new Date();
-let year = document.querySelector(".year");
-year.innerHTML = `${ date.getFullYear() }`;
+// let date = new Date();
+// let year = document.querySelector(".year");
+// year.innerHTML = `${ date.getFullYear() }`;
 
 //fetch products from server
 let products=document.querySelectorAll(".products");
@@ -33,11 +33,15 @@ fetch("productData/product.json").then(
     (request)=>{return request.json()}
 ).then(
     (data)=>{  
-        for(let x=0;x<2;x++){      
-        for(let i=0;i<4;i++){
-            let random=Math.floor(Math.random()*data.length);  
-            products[x].innerHTML+=
-            `<div class="card">  
+        for (let x = 0; x < 2; x++){  
+            
+            for (let i = 0; i < 4; i++){
+            
+                let random = Math.floor(Math.random() * data.length);  
+                
+                products[x].innerHTML +=
+                
+            `<div class="Card">  
             <img src="${data[random].product_img}" alt="" class="product-img"> 
              <span class="product-brand">${data[random].product_brand}</span>
              <h3 class="product-name">${data[random].product_name}</h3>
@@ -53,9 +57,42 @@ fetch("productData/product.json").then(
                <i class="fa-solid fa-cart-shopping"></i>
              </div>
            </div>`;
+                
             }
 
         }
     }
 )
 
+
+//add products to shop page
+
+let shop=document.querySelector(".shop");
+fetch("productData/product.json").then(
+    (request)=>{return request.json()}
+).then(
+    (data)=>{  
+        for (let x = 0; x <data.length; x++){                 
+          shop.innerHTML += `        
+            <div class="Card">  
+            <img src="${data[x].product_img}" alt="" class="product-img"> 
+             <span class="product-brand">${data[x].product_brand}</span>
+             <h3 class="product-name">${data[x].product_name}</h3>
+             <div class="product-stars">
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+               <i class="fa-solid fa-splotch"></i>
+             </div>
+             <p class="product-price">${data[x].product_price}EG</p>
+             <div class="product-buy">
+               <i class="fa-solid fa-cart-shopping"></i>
+             </div>
+           </div>`;
+                
+     }
+
+        }
+    
+)
